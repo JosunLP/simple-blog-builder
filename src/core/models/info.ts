28504d3d6da224298d643261950f2b1a63ccde4e
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2024-03-22 14:24:38 +0100

    release Script

--- src/core/models/info.ts
+++ src/core/models/info.ts
@@ -12,7 +12,7 @@ export default class Info {
 	/**
 	 * Version  of info
 	 */
-	private readonly VERSION: string = "1.0.1";
+	private readonly VERSION: string = "1.0.3";
 
 	/**
 	 * Author  of info
