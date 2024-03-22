d5c854ae73e09ab1479d426e68b5ccfbd1cb24dd
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2024-03-22 14:12:51 +0100

    working on export

--- src/core/models/BlogEntry.ts
+++ src/core/models/BlogEntry.ts
@@ -1,3 +1,22 @@
+/**
+ * BlogEntry model
+ * @class
+ * @public
+ * @property {string} title - The title of the blog entry
+ * @property {string} date - The date of the blog entry
+ * @property {string} author - The author of the blog entry
+ * @property {string[]} tags - The tags of the blog entry
+ * @property {string} content - The content of the blog entry
+ *
+ * @example
+ * const blogEntry = new BlogEntry(
+ * 	"Hello World",
+ * 	"2021-01-01",
+ * 	"John Doe",
+ * 	["hello", "world"],
+ * 	"Hello, world!"
+ * );
+ */
 export class BlogEntry {
 	public title: string;
 	public date: string;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Core
 export { default as SBBCore } from "./core/SimpleBlogBuilder";
-export { ConfigObject as SBBCoreConfig } from "./core/types/ConfigObject.type";
+export type { ConfigObject as SBBCoreConfig } from "./core/types/ConfigObject.type";
 export { BlogEntry as SBBCoreBlogEntry } from "./core/models/BlogEntry";
